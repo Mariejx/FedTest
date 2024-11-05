0f5203f37704c446bccb50a278207191bb3cb9a0
Mariejx <152178389+Mariejx@users.noreply.github.com>
2024-11-05 23:12:09 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+
+/************************************/
+/* menu openen met de open menu button */
+/************************************/
+
+var openButton = document.querySelector("header > button");
+
+openButton.onclick = openMenu; 
+
+function openMenu() {  
+  var deNav = document.querySelector("nav");
+  deNav.classList.add("toonMenu");
+}
+
+
+
+/************************************/
+/* menu sluiten met de sluit button */
+/************************************/
+
+var sluitButton = document.querySelector("nav button");
+
+sluitButton.onclick = sluitMenu;
+
+
+function sluitMenu() {
+  var deNav = document.querySelector("nav");
+  deNav.classList.remove("toonMenu");
+}
+
+
+/*hamburger menu - https://codepen.io/shooft/pen/VwJXNEg */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
